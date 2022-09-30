c8eaa515c998bf53eaf472ec1cf9711b5b835811
leo153624nid <barjastroy@gmail.com>
2022-09-30 18:57:43 +0300

    add typings.d.ts

---
+++ src/components/App/App.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable import/no-unresolved */
+import React from 'react'
+import s from './App.module.css'
+
+function App() {
+    return (
+        <div className={s.app}>
+            <h1>ToDo List</h1>
+            <section>1</section>
+            <section>2</section>
+        </div>
+    )
+}
+
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import './index.css'
-import App from './App'
+import App from './components/App/App'
 import store from './store/reduxStore'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
