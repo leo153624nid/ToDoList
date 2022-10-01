ab8206d46a73abce93d1902caa0409e01718c0c4
leo153624nid <barjastroy@gmail.com>
2022-10-01 15:44:20 +0300

    Create Button.tsx

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,26 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
+import React from 'react'
+import s from './Button.module.css'
+
+interface ButtonProps extends React.ComponentPropsWithRef<'button'> {
+    color: 'orange' | 'blue' | 'red'
+}
+
+function Button({ children, color, onClick, ...props }: ButtonProps) {
+    const className = `${s.button} ${s[`button_${color}`]}`
+
+    return (
+        <button
+            className={className}
+            type="button"
+            onClick={onClick}
+            // eslint-disable-next-line react/jsx-props-no-spreading
+            {...props}
+        >
+            {children}
+        </button>
+    )
+}
+
+export default Button
