02d1b48d723b5a594bb11be400015160fb0f48ea
leo153624nid <barjastroy@gmail.com>
2022-10-01 16:51:37 +0300

    Update TaskItem.tsx

--- src/components/TaskItem/TaskItem.tsx
+++ src/components/TaskItem/TaskItem.tsx
@@ -2,6 +2,7 @@
 /* eslint-disable import/no-unresolved */
 import React from 'react'
 import { Task } from '../../store/slices/todoListSlice'
+import Button from '../Button/Button'
 import s from './TaskItem.module.css'
 
 interface TaskItemProps {
@@ -11,7 +12,26 @@ interface TaskItemProps {
 function TaskItem({ task }: TaskItemProps) {
     return (
         <div className={s.taskItem_container}>
-            <div>{task.title}</div>
+            <div>
+                <div
+                    aria-hidden
+                    className={s.taskItem_title}
+                    style={{
+                        opacity: task.done ? 0.5 : 1,
+                        textDecoration: task.done ? 'line-through' : 'none',
+                    }}
+                >
+                    {task.title}
+                </div>
+                <div aria-hidden className={s.taskItem_description}>
+                    {task.description}
+                </div>
+            </div>
+            <div className={s.taskItem_button_container}>
+                <Button color="green">DONE</Button>
+                <Button color="orange">EDIT</Button>
+                <Button color="red">DELETE</Button>
+            </div>
         </div>
     )
 }
