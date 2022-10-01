3652c0ce528b2cfcde1ffa611f80b2558437ef17
leo153624nid <barjastroy@gmail.com>
2022-10-01 14:30:01 +0300

    update css

--- src/components/Panel/Panel.tsx
+++ src/components/Panel/Panel.tsx
@@ -3,6 +3,7 @@
 import React, { useState } from 'react'
 import { Task } from '../../store/slices/todoListSlice'
 import { generateId } from '../../utils/GenerateId'
+import s from './Panel.module.css'
 
 const DEFAULT_TASK_ITEM: Task = {
     id: '',
@@ -21,32 +22,34 @@ function Panel() {
     }
 
     return (
-        <div>
-            <div>
-                <label htmlFor="title">
-                    <div>title</div>
-                    <input
-                        type="text"
-                        id="title"
-                        name="title"
-                        value={taskItem.title}
-                        onChange={onChange}
-                    />
-                </label>
+        <div className={s.panel_container}>
+            <div className={s.fields_container}>
+                <div className={s.field_container}>
+                    <label htmlFor="title">
+                        <div>title</div>
+                        <input
+                            type="text"
+                            id="title"
+                            name="title"
+                            value={taskItem.title}
+                            onChange={onChange}
+                        />
+                    </label>
+                </div>
+                <div className={s.field_container}>
+                    <label htmlFor="description">
+                        <div>description</div>
+                        <input
+                            type="text"
+                            id="description"
+                            name="description"
+                            value={taskItem.description}
+                            onChange={onChange}
+                        />
+                    </label>
+                </div>
             </div>
-            <div>
-                <label htmlFor="description">
-                    <div>description</div>
-                    <input
-                        type="text"
-                        id="description"
-                        name="description"
-                        value={taskItem.description}
-                        onChange={onChange}
-                    />
-                </label>
-            </div>
-            <div>
+            <div className={s.button_container}>
                 <button type="button">ADD</button>
             </div>
         </div>
