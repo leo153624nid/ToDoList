6f62fcf6883e729ccb32b1c10af5825b5c5d0b7d
leo153624nid <barjastroy@gmail.com>
2022-10-01 18:04:25 +0300

    Update List.tsx

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,17 +1,27 @@
 /* eslint-disable import/extensions */
 /* eslint-disable import/no-unresolved */
-import React from 'react'
+import React, { useState } from 'react'
 import { useAppSelector } from '../../store/hooks/hooks'
+import { Task } from '../../store/slices/todoListSlice'
+import Panel from '../Panel/Panel'
 import TaskItem from '../TaskItem/TaskItem'
 import s from './List.module.css'
 
 function List() {
     const { tasks } = useAppSelector((state) => state.todoList)
+    const [taskForEdit, setTaskForEdit] = useState<Task | null>(null)
     return (
         <div className={s}>
-            {tasks.map((elem) => (
-                <TaskItem key={elem.id} task={elem} />
-            ))}
+            {tasks.map((elem) => {
+                if (elem.id === taskForEdit?.id) return <Panel key={elem.id} />
+                return (
+                    <TaskItem
+                        key={elem.id}
+                        task={elem}
+                        setTaskForEdit={setTaskForEdit}
+                    />
+                )
+            })}
         </div>
     )
 }
