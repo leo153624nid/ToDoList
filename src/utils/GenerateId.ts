e966f56893bc248f7f8154b4e46109c364cfa0c2
leo153624nid <barjastroy@gmail.com>
2022-10-01 13:04:56 +0300

    add GenerateId

---
+++ src/utils/GenerateId.ts
@@ -0,0 +1,5 @@
+/* eslint-disable import/prefer-default-export */
+type GenerateId = () => string
+
+export const generateId: GenerateId = () =>
+    Math.random().toString(16).slice(2) + new Date().getTime().toString(36)
